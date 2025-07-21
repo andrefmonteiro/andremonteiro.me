1c058a2958756a2b60155fc04e15b3aaeafc3fe0
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-21 22:16:10 +0100

    tailwind file created; eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+  eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+			},
+		},
+	},
 })
