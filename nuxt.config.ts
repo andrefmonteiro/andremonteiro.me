b757bb736612b639a27cc1d6e748cacd385e64cf
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-23 23:44:57 +0100

    @nuxt/tailwind module removed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
-	// Add this line
+	modules: ['@nuxt/eslint'],
 	devtools: { enabled: true },
 	css: ['~/assets/css/tailwind.css'],	compatibilityDate: '2025-07-15',
 	eslint: {
