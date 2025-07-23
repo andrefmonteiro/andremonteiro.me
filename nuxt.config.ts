46a11da075faa8eb9420e884db7a3c98204c581b
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-23 23:56:58 +0100

    tailwind vite working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint'],
 	devtools: { enabled: true },
-	css: ['~/assets/css/tailwind.css'],	compatibilityDate: '2025-07-15',
+	css: ['~/assets/css/main.css'],	compatibilityDate: '2025-07-15',
 	vite: {
 		plugins: [
 			tailwindcss(),
