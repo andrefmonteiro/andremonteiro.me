cd40aba7f883d6fc71e6346d75b50c67043e2077
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-23 23:47:58 +0100

    tailwind vite dependency installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 	modules: ['@nuxt/eslint'],
 	devtools: { enabled: true },
 	css: ['~/assets/css/tailwind.css'],	compatibilityDate: '2025-07-15',
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 	eslint: {
 		config: {
 			stylistic: {
