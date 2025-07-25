e7d76909de3a9706376963a59cb9e0e8ef2d6513
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-25 16:42:19 +0100

    aside component structure

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,6 @@
 <template>
-	<div class="bg-red-400 flex flex-col md:flex-row max-w-5xl mb-40 mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
-		app.vue
-		<!-- AsideContent -->
+	<div class="bg-neutral-700 flex flex-col md:flex-row max-w-5xl mb-40 mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+		<AsideContent />
 		<!-- MainContent with NuxtPage -->
 	</div>
 </template>
--- app/components/AsideContent.vue
+++ app/components/AsideContent.vue
@@ -1,10 +1,25 @@
 <template>
-	<div>
-		AsideContent
-		<!--
-			- logo
-			- name (hidden on desktop)
-			- navbar
-			-->
-	</div>
+	<aside class="md:w-[200px] md:flex-shrink-0 md:mx-0 md:px-0">
+		<div class="lg:sticky lg:top-0">
+			<span class="bg-blue-500 text-white w-9 h-9 rounded-full flex items-center justify-center">AM</span>
+			<NuxtLink
+				to="/"
+				class="md:hidden"
+			>André Monteiro</NuxtLink>
+			<nav class="flex flex-row md:flex-col text-sm space-x-4 md:space-x-0 md:space-y-4">
+				<NuxtLink
+					to="/about"
+					class="hover:opacity-60 py-1"
+				>About</NuxtLink>
+				<NuxtLink
+					to="/blog"
+					class="hover:opacity-60 py-1"
+				>Blog</NuxtLink>
+				<NuxtLink
+					to="/portfolio"
+					class="hover:opacity-60 py-1"
+				>Portfolio</NuxtLink>
+			</nav>
+		</div>
+	</aside>
 </template>
