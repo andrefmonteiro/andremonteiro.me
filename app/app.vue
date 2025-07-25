98b0d67b426ce582f6f0c6b7997726b7a4816f14
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-26 00:34:28 +0100

    refactor: styling centralized in app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class=" border-red-500 flex flex-col md:flex-row max-w-5xl mb-40 mx-4 mt-8 md:mt-32">
-		<AsideContent />
-		<MainContent />
+	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
+		<div class="flex flex-col mt-8 mb-40 md:flex-row md:mt-32">
+			<AsideContent />
+			<MainContent />
+		</div>
+		<AppFooter />
 	</div>
 </template>
--- app/components/AsideContent.vue
+++ app/components/AsideContent.vue
@@ -1,36 +1,36 @@
 <template>
-	<aside class="md:w-[200px] md:flex-shrink-0 -mx-4 md:mx-3 md:px-0">
+	<aside class="md:w-[200px] md:flex-shrink-0">
 		<div class="lg:sticky lg:top-0">
-			<div class="mb-2 px-4 md:px-0 md:mb-10 space-y-8 flex flex-col md:flex-row items-start ">
+			<div class="flex flex-col items-start mb-2 space-y-8 md:mb-10 md:flex-row ">
 				<NuxtLink to="/">
-					<span class="bg-accent-green text-white w-9 h-9 rounded-full flex items-center justify-center ">AM</span>
+					<span class="flex items-center justify-center text-white rounded-full bg-accent-green w-9 h-9 ">AM</span>
 				</NuxtLink>
-				<h1 class="md:hidden uppercase">
+				<h1 class="uppercase md:hidden">
 					<NuxtLink to="/">André Monteiro</NuxtLink>
 				</h1>
 			</div>
 
-			<nav class="flex flex-row md:flex-col items-start relative md:top-[-7px] overflow-scroll px-4 md:px-0 pb-4 md:pb-0 md:overflow-auto scroll-pr-6 md:relative md:left-[-6px]">
-				<div class="flex flex-row md:flex-col space-x-4 md:space-x-0 pr-10">
-					<div class="flex flex-col-reverse md:flex-row text-sm py-1 items-center">
+			<nav class="flex flex-row md:flex-col items-start relative md:top-[-7px] md:px-0 pb-4 md:pb-0 md:relative md:left-[-6px]">
+				<div class="flex flex-row pr-10 space-x-4 md:flex-col md:space-x-0">
+					<div class="flex flex-col-reverse items-center py-1 text-sm md:flex-row">
 						<div class="md:relative rounded-full md:mr-2 md:left-[1px] w-[4px] h-[4px] mt-1 md:mt-0 transition-transform scale-125 bg-accent-green" />
 						<NuxtLink
 							to="/"
-							class="transition-opacity hover:opacity-60 no-underline"
+							class="no-underline transition-opacity hover:opacity-60"
 						>About</NuxtLink>
 					</div>
-					<div class="flex flex-col-reverse md:flex-row text-sm py-1 items-center">
+					<div class="flex flex-col-reverse items-center py-1 text-sm md:flex-row">
 						<div class="md:relative rounded-full md:mr-2 md:left-[1px] w-[4px] h-[4px] mt-1 md:mt-0 transition-transform scale-125 bg-accent-green" />
 						<NuxtLink
 							to="/blog"
-							class="transition-opacity hover:opacity-60 no-underline"
+							class="no-underline transition-opacity hover:opacity-60"
 						>Blog</NuxtLink>
 					</div>
-					<div class="flex flex-col-reverse md:flex-row text-sm py-1 items-center">
+					<div class="flex flex-col-reverse items-center py-1 text-sm md:flex-row">
 						<div class="md:relative rounded-full md:mr-2 md:left-[1px] w-[4px] h-[4px] mt-1 md:mt-0 transition-transform scale-125 bg-accent-green" />
 						<NuxtLink
 							to="/portfolio"
-							class="transition-opacity hover:opacity-60 no-underline"
+							class="no-underline transition-opacity hover:opacity-60"
 						>Portfolio</NuxtLink>
 					</div>
 				</div>
