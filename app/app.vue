3f754840a0e3821ab9e56492b98fa852196105ee
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-26 02:39:41 +0100

    pixel-perfecting layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
-		<div class="flex flex-col md:flex-row mt-8 md:mt-32 mb-40 ">
+		<div class="flex flex-col md:grid md:grid-cols-[220px_1fr] mt-8 md:mt-32 mb-40 ">
 			<AsideContent />
 			<MainContent />
 		</div>
--- app/components/MainContent.vue
+++ app/components/MainContent.vue
@@ -1,15 +1,11 @@
 <template>
-	<div class="mt-6 md:mt-0">
-		<h1 class="hidden md:block uppercase mb-2 md:mb-20">
+	<main>
+		<!-- Desktop heading -->
+		<h1 class="hidden md:block uppercase mb-8">
 			<NuxtLink to="/">André Monteiro</NuxtLink>
 		</h1>
-		<!--
-			- PageDetails (conditionally rendered)
-			- MainContent
-			-->
-		<div class="md:pt-[calc(2.25rem+2rem+0.5rem)]">
-			<!-- Logo height + space-y-8 + mb-2 -->
-			<NuxtPage />
-		</div>
-	</div>
+
+		<!-- Main content area -->
+		<NuxtPage />
+	</main>
 </template>
