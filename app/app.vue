bd28f1e68fc525bafe8b3785e0548ee34c3a76e6
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-28 18:21:14 +0100

    sticky logo + nav implemented

--- app/app.vue
+++ app/app.vue
@@ -9,10 +9,9 @@ const hasPageDetails = computed(() => {
 <template>
 	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
 		<div class="layout-container">
-			<HeaderLogo class="header-logo md:pt-1" />
+			<HeaderLogo class="header-logo md:pt-1 md:sticky md:top-28" />
 			<HeaderName class="header-name" />
-			<AppNavbar class="app-navbar md:pt-1.5" />
-
+			<AppNavbar class="app-navbar md:sticky md:top-64 md:-ml-3" />
 			<div class="main-content">
 				<PageDetails v-if="hasPageDetails" />
 				<NuxtPage />
