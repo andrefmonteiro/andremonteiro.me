0ec85a0da1e5aa5986b85699a0e6f8bf5e4837dd
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-28 20:01:18 +0100

    tailwind

--- app/app.vue
+++ app/app.vue
@@ -9,9 +9,9 @@ const hasPageDetails = computed(() => {
 <template>
 	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
 		<div class="layout-container">
-			<HeaderLogo class="header-logo mb-8 md:pt-1 md:sticky md:top-28" />
+			<HeaderLogo class="header-logo mb-8 md:mb-0" />
 			<HeaderName class="header-name mb-4" />
-			<AppNavbar class="app-navbar md:sticky md:top-64 md:-ml-3 mb-8" />
+			<AppNavbar class="app-navbar md:sticky md:top-64 md:-ml-3 mb-8 md:pt-1" />
 			<main class="main-content">
 				<PageDetails v-if="hasPageDetails" />
 				<NuxtPage />
