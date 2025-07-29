11869d71de45cb0e44f0c244583281e7056093fe
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-29 18:17:57 +0100

    icons added

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,8 +1,10 @@
 <template>
 	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
 		<p>© André Monteiro.</p>
-		<div>
-			email github linkedin
+		<div class="flex gap-x-4">
+			<Icon name="grommet-icons:mail" />
+			<Icon name="grommet-icons:linkedin" />
+			<Icon name="grommet-icons:github" />
 		</div>
 	</footer>
 </template>
