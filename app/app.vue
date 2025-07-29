4723ab1215963fff53eaff3cb4690ef503473a79
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-29 16:56:28 +0100

    final(?) alignment; footer layout added

--- app/app.vue
+++ app/app.vue
@@ -7,9 +7,9 @@ const hasPageDetails = computed(() => {
 </script>
 
 <template>
-	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
-		<div class="flex flex-col gap-8 mt-6 mb-40 md:flex-row md:mt-28 md:items-start">
-			<div class="flex flex-col md:w-[200px] md:flex-shrink-0 md:gap-24 md:sticky md:top-0 md:self-start">
+	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto mb-30">
+		<div class="flex flex-col gap-8 mt-6 md:flex-row md:mt-28 md:items-start mb-20">
+			<div class="flex flex-col md:w-[200px] md:flex-shrink-0 md:gap-24 md:sticky md:top-28 md:self-start">
 				<HeaderLogo class="mb-6 md:mb-0" />
 				<HeaderName class="md:hidden mb-2 md:mb-0" />
 				<AppNavbar />
@@ -23,6 +23,6 @@ const hasPageDetails = computed(() => {
 				</main>
 			</div>
 		</div>
-		<AppFooter />
+		<AppFooter class="md:ml-58" />
 	</div>
 </template>
--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,8 @@
 <template>
-	<p>Footer</p>
+	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
+		<p>© André Monteiro.</p>
+		<div>
+			email github linkedin
+		</div>
+	</footer>
 </template>
