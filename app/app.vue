992239a62f7a1ea4c6c8997e26866e4a3aa34222
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-29 13:42:52 +0100

    flexbox implemented

--- app/app.vue
+++ app/app.vue
@@ -8,14 +8,21 @@ const hasPageDetails = computed(() => {
 
 <template>
 	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto">
-		<div class="layout-container">
-			<HeaderLogo class="header-logo mb-8 md:mb-0" />
-			<HeaderName class="header-name mb-4" />
-			<AppNavbar class="app-navbar md:sticky md:top-64 mb-8 md:pt-1" />
-			<main class="main-content">
-				<PageDetails v-if="hasPageDetails" />
-				<NuxtPage />
-			</main>
+		<div class="flex flex-col gap-8 mt-8 mb-40 md:flex-row md:mt-28 md:items-start">
+			<div class="flex flex-col gap-8 md:w-[200px] md:flex-shrink-0 md:gap-24 md:sticky md:top-0 md:self-start">
+				<HeaderLogo class="logo" />
+				<HeaderName class="md:hidden" />
+				<AppNavbar class="navbar" />
+			</div>
+
+			<div class="flex flex-col gap-8 md:flex-1 md:gap-0">
+				<HeaderName class="hidden md:block md:mb-24" />
+
+				<main class="content">
+					<PageDetails v-if="hasPageDetails" />
+					<NuxtPage />
+				</main>
+			</div>
 		</div>
 		<AppFooter />
 	</div>
