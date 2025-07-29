cac6882de94996de335aa6cf9dee347598122919
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-29 18:26:28 +0100

    resized footer icons

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -2,9 +2,18 @@
 	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
 		<p>© André Monteiro.</p>
 		<div class="flex gap-x-4">
-			<Icon name="grommet-icons:mail" />
-			<Icon name="grommet-icons:linkedin" />
-			<Icon name="grommet-icons:github" />
+			<Icon
+				name="grommet-icons:mail"
+				size="18px"
+			/>
+			<Icon
+				name="grommet-icons:linkedin"
+				size="18px"
+			/>
+			<Icon
+				name="grommet-icons:github"
+				size="18px"
+			/>
 		</div>
 	</footer>
 </template>
