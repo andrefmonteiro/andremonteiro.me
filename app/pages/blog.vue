b71426f007fa3f6b882d63c623176ad16b5bc318
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-30 23:55:35 +0100

    mock data for prop testing

--- app/pages/blog.vue
+++ app/pages/blog.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import BlogPostCard from '../components/BlogPostCard.vue'
+</script>
+
 <template>
 	<div>
-		<p>Blog page</p>
+		<!-- these props will be parsed from each blog post yaml frontmatter -->
+		<BlogPostCard
+			title="The case for learning functional programming The case for learning functional programming  The case for learning functional programming  The case for learning functional programming "
+			date="July 30, 2025"
+		/>
+		<BlogPostCard
+			title="Why you should try guatemalan coffee"
+			date="July 30, 2025"
+		/>
 	</div>
 </template>
