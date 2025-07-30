47cc5adb3e2fee40eca0492657a8421a9fd69982
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-31 00:06:59 +0100

    layout changes

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav class="flex flex-row md:flex-col items-start text-sm">
 		<div class="flex flex-row space-x-6 md:flex-col md:space-x-0 md:space-y-2 md:-ml-2">
-			<div class="flex flex-col-reverse md:flex-row items-center py-1">
+			<div class="flex flex-col-reverse md:flex-row items-center py-1 text-accent-green">
 				<div class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0 bg-accent-green" />
 				<NuxtLink
 					to="/"
