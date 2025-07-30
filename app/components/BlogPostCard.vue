3f3c8bf031c923df66571cb85ba7cec094663716
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-31 00:37:09 +0100

    styling: heading color added; fixed padding on blog post date

--- app/components/BlogPostCard.vue
+++ app/components/BlogPostCard.vue
@@ -4,12 +4,12 @@ const props = defineProps({ title: String, date: String })
 
 <template>
 	<article class="flex flex-col md:flex-row justify-between md:gap-8 border-b border-surface-muted last:border-b-0 pt-1.5 pb-1.5 first:pt-0">
-		<h2 class="flex-1 text-xl hover:text-text-secondary transition">
+		<h2 class="flex-1 text-xl font-medium text-text-heading hover:text-text-primary transition">
 			{{ props.title }}
 		</h2>
 
 		<time
-			class="text-text-secondary text-sm"
+			class="text-text-secondary text-sm pt-1.5"
 			:datetime="props.date"
 		>
 			{{ props.date }}
--- app/components/HeaderName.vue
+++ app/components/HeaderName.vue
@@ -1,5 +1,5 @@
 <template>
-	<p class="uppercase text-xl font-stretch-expanded font-bold">
+	<p class="uppercase text-xl font-stretch-expanded font-bold text-text-heading">
 		<NuxtLink to="/">André Monteiro</NuxtLink>
 	</p>
 </template>
