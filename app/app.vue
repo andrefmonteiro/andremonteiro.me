d24f18170cf86259b9343fd80f4f1777f41ba7e6
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-30 22:59:00 +0100

    layout changes

--- app/app.vue
+++ app/app.vue
@@ -7,22 +7,20 @@ const hasPageDetails = computed(() => {
 </script>
 
 <template>
-	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto mb-30">
-		<div class="flex flex-col gap-8 mt-6 md:flex-row md:mt-28 md:items-start mb-20">
-			<div class="flex flex-col md:w-[200px] md:flex-shrink-0 md:gap-24 md:sticky md:top-28 md:self-start">
-				<HeaderLogo class="mb-6 md:mb-0" />
-				<HeaderName class="md:hidden mb-2 md:mb-0" />
-				<AppNavbar />
-			</div>
-
-			<div class="flex flex-col gap-8 md:gap-26 md:flex-1 md:pt-1">
-				<HeaderName class="hidden md:block" />
-				<main>
-					<PageDetails v-if="hasPageDetails" />
-					<NuxtPage />
-				</main>
-			</div>
+	<div class="max-w-5xl mx-4 md:px-6 md:mx-auto mb-32 flex flex-col md:flex-row md:items-start gap-4 mt-6 md:mt-28">
+		<div class="flex flex-col md:w-[200px] md:flex-shrink-0 md:gap-24 md:sticky md:top-28 md:self-start">
+			<HeaderLogo class="mb-6 md:mb-0" />
+			<HeaderName class="md:hidden mb-2 md:mb-0" />
+			<AppNavbar />
 		</div>
-		<AppFooter class="md:ml-58" />
+
+		<div class="flex flex-col gap-8 md:gap-26 md:flex-1 md:pt-1">
+			<HeaderName class="hidden md:block" />
+			<main>
+				<PageDetails v-if="hasPageDetails" />
+				<NuxtPage />
+			</main>
+			<AppFooter />
+		</div>
 	</div>
 </template>
