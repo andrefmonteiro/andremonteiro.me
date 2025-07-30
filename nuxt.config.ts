2431ffb6bf8588e9a44e3c8df2a84705aaa5922c
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-30 18:15:59 +0100

    lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-				modules: ['@nuxt/eslint', '@nuxt/icon'],
-				devtools: { enabled: true },
-				css: ['~/assets/css/main.css'],	compatibilityDate: '2025-07-15',
-				vite: {
-								plugins: [
-												tailwindcss(),
-								],
-				},
-				eslint: {
-								config: {
-												stylistic: {
-																indent: 'tab',
-												},
-								},
-				},
+	modules: ['@nuxt/eslint', '@nuxt/icon'],
+	devtools: { enabled: true },
+	css: ['~/assets/css/main.css'],	compatibilityDate: '2025-07-15',
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
+	eslint: {
+		config: {
+			stylistic: {
+				indent: 'tab',
+			},
+		},
+	},
 })
