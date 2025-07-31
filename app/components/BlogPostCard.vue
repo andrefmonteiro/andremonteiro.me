a768f13938ef15ea54400e735ed811f115e6a393
Andre Monteiro <andrefmonteiro@outlook.com>
2025-07-31 16:26:14 +0100

    styling

--- app/components/BlogPostCard.vue
+++ app/components/BlogPostCard.vue
@@ -3,8 +3,8 @@ const props = defineProps({ title: String, date: String })
 </script>
 
 <template>
-	<article class="flex flex-col md:flex-row justify-between md:gap-8 border-b border-surface-muted last:border-b-0 pt-1.5 pb-1.5 first:pt-0">
-		<h2 class="flex-1 text-xl font-medium text-text-heading hover:text-text-primary transition">
+	<article class="flex flex-col md:flex-row justify-between md:gap-8 border-b-4 border-surface-muted last:border-b-0 pt-2 pb-2 first:pt-0">
+		<h2 class="flex-1 text-xl font-medium text-text-heading transition hover:text-text-primary">
 			{{ props.title }}
 		</h2>
 
