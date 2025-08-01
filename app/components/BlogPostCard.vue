4da1fcdd308937449341ad894449c0d06ea06b5e
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-01 15:57:20 +0100

    random greens as per usual

--- app/components/BlogPostCard.vue
+++ app/components/BlogPostCard.vue
@@ -3,7 +3,7 @@ const props = defineProps({ title: String, date: String })
 </script>
 
 <template>
-	<article class="flex flex-col md:flex-row justify-between md:gap-8 border-b-4 border-surface-muted last:border-b-0 pt-2 pb-2 first:pt-0">
+	<article class="flex flex-col md:flex-row justify-between md:gap-8 border-b-1 border-surface-muted last:border-b-0 pt-3 pb-3 first:pt-0">
 		<h2 class="flex-1 text-xl font-medium text-text-heading transition hover:text-text-primary">
 			{{ props.title }}
 		</h2>
