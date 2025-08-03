8dba0230cb53347d2f81b845d6cc821a8793cc76
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-03 19:19:14 +0100

    zod schema defined for each collection

--- content.config.ts
+++ content.config.ts
@@ -1,14 +1,23 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
 	collections: {
 		blog: defineCollection({
 			type: 'page',
 			source: 'blog/*.md',
+			schema: z.object({
+				title: z.string(),
+				date: z.string(),
+			}),
 		}),
 		portfolio: defineCollection({
 			type: 'page',
 			source: 'portfolio/*.md',
+			schema: z.object({
+				title: z.string(),
+				date: z.string(),
+				tags: z.array(z.string()),
+			}),
 		}),
 	},
 })
