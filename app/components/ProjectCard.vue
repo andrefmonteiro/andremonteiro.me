b4ff71eb30f544b588b93ae3527c141e5cfbf460
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-05 19:43:56 +0100

    project card scaffolding

---
+++ app/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+	title: String,
+	description: String,
+	techStack: Array<string>,
+	liveUrl = String,
+	githubUrl = String,
+})
+</script>
+
+<template>
+	<div class="border border-surface-muted">
+		<h2>{{ props.title }}</h2>
+		<p>{{ props.description }}</p>
+		<div>
+			<div :v-for="tool in techStack">
+				{{ tool }}
+			</div>
+		</div>
+	</div>
+</template>
