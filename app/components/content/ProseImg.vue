6545314e635a11a9c13bc6764d4368ad8c9267e5
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-06 18:02:27 +0100

    ProseImg component overwritten to use NuxtImg and force alt text

---
+++ app/components/content/ProseImg.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+const props = defineProps({
+	src: {
+		type: String,
+		required: true,
+	},
+	alt: {
+		type: String,
+		required: true,
+	},
+	title: {
+		type: String,
+		default: null,
+	},
+	width: {
+		type: [String, Number],
+		default: undefined,
+	},
+	height: {
+		type: [String, Number],
+		default: undefined,
+	},
+})
+</script>
+
+<template>
+	<figure
+		v-if="title"
+		class="mb-6"
+	>
+		<NuxtImg
+			:src="props.src"
+			:alt="props.alt"
+			:width="props.width"
+			:height="props.height"
+			preset="default"
+			loading="lazy"
+			class="max-w-full h-auto mx-auto rounded-sm"
+		/>
+		<figcaption class="text-sm text-text-tertiary text-center pt-1">
+			{{ title }}
+		</figcaption>
+	</figure>
+
+	<NuxtImg
+		v-else
+		:src="src"
+		:alt="alt"
+		:width="width"
+		:height="height"
+		preset="default"
+		loading="lazy"
+		class="mb-6 max-w-full h-auto mx-auto block rounded-lg"
+	/>
+</template>
