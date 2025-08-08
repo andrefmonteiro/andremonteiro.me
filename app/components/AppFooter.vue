5c6df718e71d6766d1a0114843da58be8bd03184
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-08 17:32:06 +0100

    footer icons are now linking

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -2,21 +2,39 @@
 	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
 		<p>© André Monteiro.</p>
 		<div class="flex gap-x-4">
-			<Icon
-				name="grommet-icons:mail"
-				size="18px"
-				class="transition hover:text-text-tertiary"
-			/>
-			<Icon
-				name="grommet-icons:linkedin"
-				size="18px"
-				class="transition hover:text-text-tertiary"
-			/>
-			<Icon
-				name="grommet-icons:github"
-				size="18px"
-				class="transition hover:text-text-tertiary"
-			/>
+			<a
+				href="mailto:andrefmonteiro@outlook.com"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon
+					name="grommet-icons:mail"
+					size="18px"
+					class="transition hover:text-text-tertiary"
+				/>
+			</a>
+			<a
+				href="https://linkedin.com/in/andrefmonteiro"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon
+					name="grommet-icons:linkedin"
+					size="18px"
+					class="transition hover:text-text-tertiary"
+				/>
+			</a>
+			<a
+				href="https://github.com/andrefmonteiro"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Icon
+					name="grommet-icons:github"
+					size="18px"
+					class="transition hover:text-text-tertiary"
+				/>
+			</a>
 		</div>
 	</footer>
 </template>
