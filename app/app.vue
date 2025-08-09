8182ce59d30870933c7f9d770ac89db9d2f94ba5
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-09 02:35:39 +0100

    fix: navbar indicates current (parent) page

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
 	<div class="max-w-4xl mx-4 md:px-6 md:ml-[14vw] md:mr-auto mb-32 flex flex-col md:flex-row md:items-start gap-10 mt-6 md:mt-28">
 		<div class="flex flex-col md:w-[175px] md:flex-shrink-0 md:gap-24 md:sticky md:top-28 md:self-start">
 			<HeaderLogo class="mb-8 md:mb-0" />
 			<HeaderName class="md:hidden mb-3 md:mb-0" />
-			<AppNavbar />
+			<AppNavbar :current-route="route.path" />
 		</div>
 
 		<div class="flex flex-col md:flex-1 md:pt-1">
--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -1,8 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+	currentRoute: {
+		type: String,
+		required: true,
+	},
+})
+
+const matchesRoute = (route: string): boolean => {
+	if (route === '/') {
+		return props.currentRoute === '/'
+	}
+	return props.currentRoute.startsWith(route)
+}
+</script>
+
 <template>
 	<nav class="flex flex-row md:flex-col items-start text-sm">
 		<div class="flex flex-row space-x-6 md:flex-col md:space-x-0 md:space-y-[6px] md:-ml-2">
-			<div class="flex flex-col-reverse md:flex-row items-center py-1 text-accent-green">
-				<div class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0 bg-accent-green" />
+			<div
+				class="flex flex-col-reverse md:flex-row items-center py-1"
+				:class="matchesRoute('/')? 'text-accent-green' : ''"
+			>
+				<div
+					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
+					:class="matchesRoute('/')? 'bg-accent-green' : 'opacity-0'"
+				/>
 				<NuxtLink
 					to="/"
 					class="transition hover:text-text-secondary"
@@ -10,8 +32,14 @@
 					About
 				</NuxtLink>
 			</div>
-			<div class="flex flex-col-reverse md:flex-row items-center py-1">
-				<div class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0 bg-accent-green" />
+			<div
+				class="flex flex-col-reverse md:flex-row items-center py-1"
+				:class="matchesRoute('/blog')? 'text-accent-green' : ''"
+			>
+				<div
+					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
+					:class="matchesRoute('/blog')? 'bg-accent-green' : 'opacity-0'"
+				/>
 				<NuxtLink
 					to="/blog"
 					class="transition hover:text-text-secondary"
@@ -19,8 +47,14 @@
 					Blog
 				</NuxtLink>
 			</div>
-			<div class="flex flex-col-reverse md:flex-row items-center py-1">
-				<div class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0 bg-accent-green" />
+			<div
+				class="flex flex-col-reverse md:flex-row items-center py-1"
+				:class="matchesRoute('/portfolio')? 'text-accent-green' : ''"
+			>
+				<div
+					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
+					:class="matchesRoute('/portfolio')? 'bg-accent-green' : 'opacity-0'"
+				/>
 				<NuxtLink
 					to="/portfolio"
 					class="transition hover:text-text-secondary"
