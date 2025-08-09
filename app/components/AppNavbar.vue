d3e364e119f40f57ca77bc760fb82fa5bfe66c70
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-09 19:47:57 +0100

    current page mark styled with a glow

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -10,15 +10,15 @@ const matchesRoute = (route: string): boolean => {
 </script>
 
 <template>
-	<nav class="flex flex-row md:flex-col items-start text-sm">
+	<nav class="flex flex-row md:flex-col items-start text-sm md:-ml-1.5">
 		<div class="flex flex-row space-x-6 md:flex-col md:space-x-0 md:space-y-[6px] md:-ml-2">
 			<div
 				class="flex flex-col-reverse md:flex-row items-center py-1"
 				:class="matchesRoute('/')? 'text-accent-green' : ''"
 			>
 				<div
-					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
-					:class="matchesRoute('/')? 'bg-accent-green' : 'opacity-0'"
+					class="w-1.5 h-1.5 mt-1 rounded-full md:mr-2 md:mt-0 transition-all duration-300"
+					:class="matchesRoute('/')? 'bg-accent-green nav-dot-active' : 'opacity-0'"
 				/>
 				<NuxtLink
 					to="/"
@@ -32,8 +32,8 @@ const matchesRoute = (route: string): boolean => {
 				:class="matchesRoute('/blog')? 'text-accent-green' : ''"
 			>
 				<div
-					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
-					:class="matchesRoute('/blog')? 'bg-accent-green' : 'opacity-0'"
+					class="w-1.5 h-1.5 mt-1 rounded-full md:mr-2 md:mt-0 transition-all duration-300"
+					:class="matchesRoute('/blog')? 'bg-accent-green nav-dot-active' : 'opacity-0'"
 				/>
 				<NuxtLink
 					to="/blog"
@@ -47,8 +47,8 @@ const matchesRoute = (route: string): boolean => {
 				:class="matchesRoute('/portfolio')? 'text-accent-green' : ''"
 			>
 				<div
-					class="w-1 h-1 mt-1 rounded-full md:mr-2 md:mt-0"
-					:class="matchesRoute('/portfolio')? 'bg-accent-green' : 'opacity-0'"
+					class="w-1.5 h-1.5 mt-1 rounded-full md:mr-2 md:mt-0 transition-all duration-300"
+					:class="matchesRoute('/portfolio')? 'bg-accent-green nav-dot-active' : 'opacity-0'"
 				/>
 				<NuxtLink
 					to="/portfolio"
