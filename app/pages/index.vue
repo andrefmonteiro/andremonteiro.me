0b4839f91552cbb4b050d34bc97f917c93fb15f0
Andre Monteiro <andrefmonteiro@outlook.com>
2025-08-25 22:20:35 +0100

    home content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,7 +11,7 @@ defineOgImageComponent('Pergel', {
 			I'm a software developer living in Porto, Portugal. I also study Computer Science at the University of Porto.
 		</p>
 		<p>
-			I work on web apps that solve someone's problem, wrapped with an intutive user experience. In the process, I enjoy learning about tradeoff decisions, data modelling, and the role of abstractions.
+			I work on web apps that solve someone's problem, wrapped with an intutive user experience. In the process, I enjoy learning about system design tradeoffs, data modelling, and the role of abstractions.
 		</p>
 		<p>I want to learn more about security driven development. I'm particularly curious about penetration testing.</p>
 		<p>I have a background in Social Psychology, and I was a technical recruiter at Prismic. I'm a basketball aficionado.</p>
