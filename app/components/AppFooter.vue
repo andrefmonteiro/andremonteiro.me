10730baade890f037b76a7bd66553efaec79d674
Andre Monteiro <andrefmonteiro@outlook.com>
2025-09-09 22:00:04 +0100

    accessibility: list items on the correct elements

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -3,19 +3,18 @@ const currYear = new Date().getFullYear()
 </script>
 
 <template>
+	<!-- Replace your footer with this: -->
 	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
 		<p>© {{ currYear }} André Monteiro.</p>
-		<div
-			class="flex gap-x-4"
-			role="list"
-			aria-label="Contacts/Links"
+		<ul
+			class="flex gap-x-4 list-none"
+			aria-label="Contact links"
 		>
-			<a
+			<li
 				href="mailto:andrefmonteiro@outlook.com"
 				target="_blank"
 				rel="noopener noreferrer"
 				aria-label="Email"
-				role="listitem"
 			>
 				<Icon
 					name="grommet-icons:mail"
@@ -23,13 +22,12 @@ const currYear = new Date().getFullYear()
 					class="transition hover:text-text-tertiary"
 					aria-hidden="true"
 				/>
-			</a>
-			<a
+			</li>
+			<li
 				href="https://linkedin.com/in/andrefmonteiro"
 				target="_blank"
 				rel="noopener noreferrer"
 				aria-label="LinkedIn (opens in new tab)"
-				role="listitem"
 			>
 				<Icon
 					name="grommet-icons:linkedin"
@@ -37,13 +35,12 @@ const currYear = new Date().getFullYear()
 					class="transition hover:text-text-tertiary"
 					aria-hidden="true"
 				/>
-			</a>
-			<a
+			</li>
+			<li
 				href="https://github.com/andrefmonteiro"
 				target="_blank"
 				rel="noopener noreferrer"
 				aria-label="GitHub (opens in new tab)"
-				role="listitem"
 			>
 				<Icon
 					name="grommet-icons:github"
@@ -51,7 +48,7 @@ const currYear = new Date().getFullYear()
 					class="transition hover:text-text-tertiary"
 					aria-hidden="true"
 				/>
-			</a>
-		</div>
+			</li>
+		</ul>
 	</footer>
 </template>
