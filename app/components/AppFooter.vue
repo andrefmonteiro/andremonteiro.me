5745191dbc107b41c6e167fce42c27abafb0667f
Andre Monteiro <andrefmonteiro@outlook.com>
2025-09-12 01:38:03 +0100

    fix: links now wrapped in achor element

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -3,51 +3,60 @@ const currYear = new Date().getFullYear()
 </script>
 
 <template>
-	<!-- Replace your footer with this: -->
 	<footer class="flex justify-between text-text-secondary text-sm md:flex-1">
 		<p>© {{ currYear }} André Monteiro.</p>
+
 		<ul
 			class="flex gap-x-4 list-none"
 			aria-label="Contact links"
 		>
-			<li
-				href="mailto:andrefmonteiro@outlook.com"
-				target="_blank"
-				rel="noopener noreferrer"
-				aria-label="Email"
-			>
-				<Icon
-					name="grommet-icons:mail"
-					size="18px"
-					class="transition hover:text-text-tertiary"
-					aria-hidden="true"
-				/>
+			<li>
+				<a
+					href="mailto:andrefmonteiro@outlook.com"
+					aria-label="Email"
+				>
+
+					<Icon
+						name="grommet-icons:mail"
+						size="18px"
+						class="transition hover:text-text-tertiary"
+						aria-hidden="true"
+					/>
+				</a>
 			</li>
-			<li
-				href="https://linkedin.com/in/andrefmonteiro"
-				target="_blank"
-				rel="noopener noreferrer"
-				aria-label="LinkedIn (opens in new tab)"
-			>
-				<Icon
-					name="grommet-icons:linkedin"
-					size="18px"
-					class="transition hover:text-text-tertiary"
-					aria-hidden="true"
-				/>
+
+			<li>
+				<a
+					href="https://linkedin.com/in/andrefmonteiro"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="LinkedIn (opens in new tab)"
+				>
+
+					<Icon
+						name="grommet-icons:linkedin"
+						size="18px"
+						class="transition hover:text-text-tertiary"
+						aria-hidden="true"
+					/>
+				</a>
 			</li>
-			<li
-				href="https://github.com/andrefmonteiro"
-				target="_blank"
-				rel="noopener noreferrer"
-				aria-label="GitHub (opens in new tab)"
-			>
-				<Icon
-					name="grommet-icons:github"
-					size="18px"
-					class="transition hover:text-text-tertiary"
-					aria-hidden="true"
-				/>
+
+			<li>
+				<a
+					href="https://github.com/andrefmonteiro"
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label="GitHub (opens in new tab)"
+				>
+
+					<Icon
+						name="grommet-icons:github"
+						size="18px"
+						class="transition hover:text-text-tertiary"
+						aria-hidden="true"
+					/>
+				</a>
 			</li>
 		</ul>
 	</footer>
