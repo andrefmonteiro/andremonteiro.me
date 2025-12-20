a3a1b5c3f40076fa2885d7af82a49adcabf88f4a
Andre Monteiro <andrefmonteiro@outlook.com>
2025-12-20 16:08:16 +0000

    content: about modded

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,12 +8,13 @@ defineOgImageComponent('Pergel', {
 <template>
 	<div>
 		<p>
-			I'm a software developer and Computer Science student based in Porto, Portugal.
+			I'm a software developer and Computer Science student at the University of Porto.
 		</p>
 		<p>
-			I like to work on applications that solve someone's problem, even if mundane, and wrap them in an intuitive user experience. I draw inspiration from using products and thinking about how they could be improved. In the process, I enjoy learning about system design tradeoffs and data modelling.
+			I like to think and work on applications that solve someone's problem, even if mundane. I draw inspiration from using products and thinking about how they could be improved. I'm interested in system design, data modelling and providing intuitive user experiences.
 		</p>
-		<p>I want to learn more about security-driven development, and I'm particularly curious about penetration testing.</p>
-		<p>I also have a background in Social Psychology, and I was a technical recruiter at Prismic. I'm a basketball aficionado.</p>
+		<p>Currently learning more about cybersecurity, with a particular interest in penetration testing.</p>
+		<p>Prior to software development, I was a technical recruiter at Prismic with a background in Social Psychology.</p>
+		<p>Will nerd out on basketball and downtempo music.</p>
 	</div>
 </template>
